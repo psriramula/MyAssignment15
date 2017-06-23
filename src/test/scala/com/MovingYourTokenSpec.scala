8f833ee199bcea4aead7702c1ca65cf0e346878a
Prasad Sriramula <prasad.sriramula@digital.hmrc.gov.uk>
2017-06-23 13:53:20 +0100

    Implemented initial Game, Token and actions to make the feature tests passing

--- src/test/scala/com/MovingYourTokenSpec.scala
+++ src/test/scala/com/MovingYourTokenSpec.scala
@@ -1,26 +1,26 @@
-package com.game
+package com
 
 import org.scalatest.mockito.MockitoSugar
-import org.scalatest.{FeatureSpec, GivenWhenThen}
+import org.scalatest.{FeatureSpec, GivenWhenThen, Matchers}
 
 
 /**
   * Created by prasadsriramula on 23/06/2017.
   */
-class MovingYourTokenSpec extends FeatureSpec with GivenWhenThen  with MockitoSugar{
+class MovingYourTokenSpec extends FeatureSpec with GivenWhenThen with Matchers{
 
 
 
   feature("Token Can Move Across the Board") {
     scenario("Starting Game and token on square 1") {
       Given("the game is started")
-      // Create a new Game
+      val game = new Game()
 
       When("the token is placed on the board")
-      // place a new token on game board
+      val token = game.createNewToken
 
       Then("the token is on square 1")
-      // assert the current position of the token
+      game.currentPosition(token) shouldBe 1
 
     }
 
